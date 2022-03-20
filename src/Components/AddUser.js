d5785df58f098b12af61270827dced1001366d4f
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2022-03-20 14:49:56 +0530

    changes

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,8 +1,46 @@
+import { FormControl, FormGroup, InputLabel,Input, Button, Typography, } from '@mui/material'
+import { makeStyles } from '@mui/styles';
 import React from 'react'
+const useStyle=makeStyles({
+  container:{
+      width:'50%',
+      margin:'5% 0 0 25%'
+  }
+})
 
+const initialValues={
+name:"",
+email:"",
+username:"",
+phone:""
+
+}
 const AddUser = () => {
+  const classes=useStyle()
   return (
-    <div>AddUser</div>
+    <>
+    
+  <FormGroup className={classes.container}>
+  <Typography variant="h4">Add User</Typography>
+    <FormControl>
+      <InputLabel>Name</InputLabel>
+      <Input/>
+    </FormControl>
+    <FormControl>
+      <InputLabel>User Name</InputLabel>
+      <Input/>
+    </FormControl>
+    <FormControl>
+      <InputLabel>Email</InputLabel>
+      <Input/>
+    </FormControl>
+    <FormControl>
+      <InputLabel>Phone</InputLabel>
+      <Input/>
+    </FormControl>
+    <Button>Add User</Button>
+  </FormGroup>
+  </> 
   )
 }
 
--- src/Components/AllUsers.js
+++ src/Components/AllUsers.js
@@ -1,17 +1,58 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getUser } from '../Services/api'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const AllUsers = () => {
+  const [user,setUsers]=useState([])
+
 
     const getAllUsers=async()=>{
       const response= await  getUser()
       console.log(response.data)
+      setUsers(response.data)
     }
     useEffect(()=>{
         getAllUsers()
-    },[])
+    },[])//component did mount
   return (
-    <div>Sayem</div>
+    <TableContainer component={Paper}>
+    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>User Name</TableCell>
+          <TableCell align="right">Email</TableCell>
+          <TableCell align="right">Id&nbsp;(g)</TableCell>
+          <TableCell align="right">Name&nbsp;(g)</TableCell>
+          <TableCell align="right">Phone&nbsp;(g)</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+      
+        {user.map((item) => (
+            console.log(user,"user"),
+          <TableRow
+            key={item.id}
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+          >
+            <TableCell component="th" scope="row">
+              {item.username}
+            </TableCell>
+            <TableCell align="right">{item.email}</TableCell>
+            <TableCell align="right">{item.id}</TableCell>
+            <TableCell align="right">{item.name}</TableCell>
+            <TableCell align="right">{item.phone}</TableCell>
+
+         </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
   )
 }
 
